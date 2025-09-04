ef6689c502ce9ec4b1823d0b70ae695706a6185d
devkbm0417 <devkbm0417@gmail.com>
2025-09-04 07:24:27 +0000

    ng2-file-uploder 시그널로 수정

--- src/app/third-party/ng2-file-upload/file-upload/file-drop.directive.ts
+++ src/app/third-party/ng2-file-upload/file-upload/file-drop.directive.ts
@@ -1,29 +1,23 @@
-import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { Directive, ElementRef, HostListener, inject, input, output } from '@angular/core';
 
 import { FileUploader, FileUploaderOptions } from './file-uploader.class';
 
-@Directive({ selector: '[ng2FileDrop]', standalone: false })
+@Directive({ selector: '[ng2FileDrop]'})
 export class FileDropDirective {
-  @Input()  uploader?: FileUploader;
-  @Output()  fileOver: EventEmitter<any> = new EventEmitter();
-  // eslint-disable-next-line @angular-eslint/no-output-on-prefix
-  @Output()  onFileDrop: EventEmitter<File[]> = new EventEmitter<File[]>();
-
-  protected element: ElementRef;
-
-  constructor(element: ElementRef) {
-    this.element = element;
-  }
-
+  readonly uploader = input<FileUploader>();
+  readonly fileOver = output<any>();  
+  readonly onFileDrop = output<File[]>();
+  protected readonly element = inject(ElementRef);
+  
   getOptions(): FileUploaderOptions | void {
-    return this.uploader?.options;
+    return this.uploader()?.options;
   }
 
   getFilters(): string {
     return '';
   }
 
-  @HostListener('drop', [ '$event' ])
+  @HostListener('drop', ['$event'])
   onDrop(event: MouseEvent): void {
     const transfer = this._getTransfer(event);
     if (!transfer) {
@@ -34,7 +28,7 @@ export class FileDropDirective {
     const filters = this.getFilters();
     this._preventAndStop(event);
     if (options) {
-      this.uploader?.addToQueue(transfer.files, options, filters);
+      this.uploader()?.addToQueue(transfer.files, options, filters);
     }
     this.fileOver.emit(false);
     this.onFileDrop.emit(transfer.files);
@@ -80,10 +74,12 @@ export class FileDropDirective {
 
     if (types.indexOf) {
       return types.indexOf('Files') !== -1;
-    } else if (types.contains) {
+    } 
+
+    if (types.contains) {
       return types.contains('Files');
-    } else {
-      return false;
-    }
+    } 
+        
+    return false;
   }
 }
--- src/app/third-party/ng2-file-upload/file-upload/file-upload.module.ts
+++ src/app/third-party/ng2-file-upload/file-upload/file-upload.module.ts
@@ -5,9 +5,14 @@ import { FileDropDirective } from './file-drop.directive';
 import { FileSelectDirective } from './file-select.directive';
 
 @NgModule({
-  imports: [ CommonModule ],
-  declarations: [ FileDropDirective, FileSelectDirective ],
-  exports: [ FileDropDirective, FileSelectDirective ]
+  imports: [
+    FileSelectDirective,
+    FileDropDirective,
+  ],
+  exports: [
+    FileSelectDirective,
+    FileDropDirective,
+  ],
 })
 export class FileUploadModule {
 }
